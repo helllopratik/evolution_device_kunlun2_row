7bd924dc4faea76679c8d2bdf9bc600c8ef070f9
HelloPratik <38004222+helllopratik@users.noreply.github.com>
2023-11-11 09:56:39 +0530

    OVERRIDE_PRODUCT_COMPRESSED_APEX:= false

--- sdm710-common/sdm710.mk
+++ sdm710-common/sdm710.mk
@@ -5,6 +5,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/sdm710-common/sdm710-common-vendor.mk)
 
